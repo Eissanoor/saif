d27e38cdfe84a803d562f81a39e7294ea9869788
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2024-01-24 13:22:31 +0500

    aaaa

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,9 @@
 const express = require("express");
-
 const adminController = require("../controllers/admin.js");
 const Response = require('../models/response.js');
-
 const router = express.Router();
-router.use(express.json()); // Add this line to parse JSON bodies
+router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
-// router.get("/add-product", adminController.getAddProduct);
 router.get('/', (req, res, next) =>
 {
     const response = new Response(200, true, "ok", null);
